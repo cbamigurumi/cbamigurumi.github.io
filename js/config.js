6e23b9028a49590dcf4866dd0b32544ddc2d8397
cbamigurumi <80424520+cbamigurumi@users.noreply.github.com>
2021-03-10 18:43:04 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -73,7 +73,7 @@ $(function() {
 	simpleStore.init({
 
 		// brand can be text or image URL
-		brand : "SimpleStore",
+		brand : "https://chikitaamigurumi.github.io/images/cb.png",
 
 		// numder of products per row (accepts 1, 2 or 3)
 		numColumns : 3,
